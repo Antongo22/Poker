2c4a5ea61ab6af05b62cd5a3b44fe51fe5b65540
Anton <80984465+Antongo22@users.noreply.github.com>
2023-12-02 17:04:23 +0300

    есть метод перемешивание куарт + заглушки для того, что нужно будет сделать

--- Poker/Cards/Cards.cs
+++ Poker/Cards/Cards.cs
@@ -15,11 +15,47 @@ namespace Poker.Cards
         public readonly static string[] suits = { "Cross", "Bubi", "Spades", "Chervi" }; //крести буби пики черви
         public readonly string[] names = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" }; // номиналы карт
 
-        public Cards() 
-        { 
+        public Cards()
+        {
             SetCards();
         }
 
+
+        /// <summary>
+        /// Обновление калоды
+        /// </summary>
+        void UpdateCards() { }
+
+        /// <summary>
+        /// Показ одной новой карты из калоды
+        /// </summary>
+        /// <param name="number">Количество карт, которые нужно показать</param>
+        void ShowCards(int number) { }
+
+        /// <summary>
+        /// Раздача карт игрокам
+        /// </summary>
+        /// <param name="countofPlayers">количество игроков</param>
+        void DealingCards(int countofPlayers) { }
+
+        /// <summary>
+        /// Перемешивание карт
+        /// </summary>
+        public void ShuffleCards()
+        {
+            Random random = new Random();
+
+            int n = cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                Card value = cards[k];
+                cards[k] = cards[n];
+                cards[n] = value;
+            }
+
+        }
 
         /// <summary>
         /// Задаём карты на поле
@@ -41,6 +77,5 @@ namespace Poker.Cards
                 }
             }
         }
-
     }
 }
